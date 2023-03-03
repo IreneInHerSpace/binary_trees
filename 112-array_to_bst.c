2e174704126878191f3f42642f4653877aeee869
IreneInHerSpace <insiteketa@gmail.com>
2023-03-04 01:15:49 +0200

    Update 112-array_to_bst.c

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,12 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert - Inserts a value in a Binary Search Tree
- * @tree: double pointer to the root node of the BST to insert the value
- * @value: Value to store in the node to be inserted
- * Return: pointer to the created node, or NULL on failure
- * if tree is NULL, the created node must become the root node.
- * If the value is already present in the tree is ignored
+ * array_to_bst - builds a Binary Search Tree from an array
+ * @array: pointer to the first element of the array to be converted
+ * @size: the number of elements in the array
+ * Return: A pointer to the root node of the created BST
+ *         NULL on failure
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
